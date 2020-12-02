802dc2fc831794ac48f4568fb7366ac3bdf062c4
Sam Johnson <sam@samwich.com>
2020-12-01 22:40:35 -0800

    day 2 puzzle 2. DATA is a File, so it can't be iterated over twice without resetting it, so I only read it once.

--- 2/day2.rb
+++ 2/day2.rb
@@ -20,9 +20,8 @@
 # How many passwords are valid according to their policies?
 
 def validate_password (input)
-  match = parse(input)
-  range = match[:from]..match[:to]
-  count = match[:password].chars.select { |x| x == match[:character] }.length
+  range = input[:from]..input[:to]
+  count = input[:password].chars.select { |x| x == input[:character] }.length
   range.include? count
 end
 
@@ -37,16 +36,43 @@ def parse (input)
   }
 end
 
-valid = 0
-DATA.each do |e|
-  valid += 1 if validate_password(e)
+PARSED_DATA = DATA.map { |d| parse d }
+
+puzzle1_answer = 0
+PARSED_DATA.each do |e|
+  puzzle1_answer += 1 if validate_password(e)
 end
 
-puts "AoC 2020 2-1: #{valid}"
+puts "AoC 2020 2-1: #{puzzle1_answer}"
 
 # Your puzzle answer was 465.
 
+# --- Part Two ---
+#
+# While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+#
+# The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+#
+# Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+#
+# Given the same example list from above:
+#
+# 1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+# 1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+# 2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+# How many passwords are valid according to the new interpretation of the policies?
 
+def validate_password_2 (input)
+  a = input[:password][input[:from]+1] == input[:character]
+  b = input[:password][input[:to]+1] == input[:character]
+  a ^ b
+end
+
+puzzle2_answer = 0
+PARSED_DATA.each do |e|
+  puzzle2_answer += 1 if validate_password_2(e)
+end
+puts "AoC 2020 2-2: #{puzzle2_answer}"
 
 __END__
 16-18 h: hhhhhhhhhhhhhhhhhh
