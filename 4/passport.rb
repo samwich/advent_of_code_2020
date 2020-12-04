a4510417ce3fd1b64318c585eb48a2732fc80153
Sam Johnson <sam@samwich.com>
2020-12-04 14:15:02 -0800

    part 2 validation tests passing

--- 4/passport.rb
+++ 4/passport.rb
@@ -11,7 +11,25 @@ class Passport
     'hgt',
   ]
 
-  
+  VALIDATORS = {
+    'byr' => ->(x) { (1920..2002).include?(x.to_i) }, # byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    'iyr' => ->(x) { (2010..2020).include?(x.to_i) }, # iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    'eyr' => ->(x) { (2020..2030).include?(x.to_i) }, # eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    'hgt' => ->(x) do              # hgt (Height) - a number followed by either cm or in:
+      if x[-2,2] == 'cm'
+        (150..193).include?(x.to_i)  # If cm, the number must be at least 150 and at most 193.
+      elsif x[-2,2] == 'in'
+        (59..76).include?(x.to_i)    # If in, the number must be at least 59 and at most 76.
+      else
+        false
+      end
+    end, 
+    'hcl' => ->(x) { /^#[0-9a-f]{6}$/.match?(x) }, # hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    'ecl' => ->(x) { /^(amb|blu|brn|gry|grn|hzl|oth)$/.match? x }, # ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    'pid' => ->(x) { /\d{9}/.match?(x) }, # pid (Passport ID) - a nine-digit number, including leading zeroes.
+    'cid' => ->(x) { true }, # cid (Country ID) - ignored, missing or not.
+  }
+
   def initialize (fields)
     @fields = fields
   end
@@ -23,6 +41,14 @@ class Passport
         return false
       end
     end
+    
+    VALIDATORS.each do |name, validator|
+      unless validator.call @fields[name]
+        puts "fail: #{name} #{@fields[name]}"
+        return false
+      end
+    end
+    
     true
   end
 end
--- 4/test_day4.rb
+++ 4/test_day4.rb
@@ -21,12 +21,21 @@ class TestDay4 < Test::Unit::TestCase
   def test_batch_record
     raw_record = BatchFile.new('./test_input').raw_records.first
     br = BatchRecord.new(raw_record)
-    pp br.fields
     assert_equal(8, br.fields.length)
   end
   
   def test_passport
     passports = BatchFile.new('./test_input').passports
     assert_equal 2, passports.map { |e| e.valid? }.count { |e| e }
+  end
+  
+  def test_good_passports
+    passports = BatchFile.new('./test_valid_passports').passports
+    assert_equal 4, passports.map { |e| e.valid? }.count { |e| e }
+  end
+  
+  def test_bad_passports
+    passports = BatchFile.new('./test_invalid_passports').passports
+    assert_equal 0, passports.map { |e| e.valid? }.count { |e| e }
   end
 end
