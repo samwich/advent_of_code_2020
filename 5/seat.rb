18447d82c230f0b916eb001a61420dcc600bb1ad
Sam Johnson <sam@samwich.com>
2020-12-05 18:02:30 -0800

    use String#to_i(2) to get seat id

--- 5/seat.rb
+++ 5/seat.rb
@@ -1,18 +1,14 @@
 class Seat
+  attr_reader :seat_id
+
+  BIN_MAP = {
+    'F' => '0',
+    'B' => '1',
+    'L' => '0',
+    'R' => '1',
+  }
+
   def initialize (code)
-    @bits = code.chars.map do |c|
-      {
-        'F' => 0,
-        'B' => 1,
-        'L' => 0,
-        'R' => 1,
-      }[c]
-    end
-  end
-  
-  def seat_id
-    @bits.reverse.each_with_index.reduce(0) do |sum, bit_exp|
-      sum += bit_exp[0] * (2 ** bit_exp[1])
-    end
+    @seat_id = code.gsub(/./, BIN_MAP).to_i(2)
   end
 end
