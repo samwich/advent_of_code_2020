7b8e6fb4fa6b41e762bbadeeaea9da76fceee640
Sam Johnson <sam@samwich.com>
2020-12-07 12:39:30 -0800

    clean up Bag

--- 7/bag.rb
+++ 7/bag.rb
@@ -1,20 +1,24 @@
 class Bag
+  attr_reader :name, :child_rules
+
   def initialize (raw_rule)
-    @raw_rule = raw_rule
+    @name, @child_rules = parse_name_and_rules(raw_rule)
   end
-  
-  
+
   # dark orange bags contain 3 bright white bags, 4 muted yellow bags.
   # bright white bags contain 1 shiny gold bag.
   # faded blue bags contain no other bags.
-  
-  
+
   def rule
-    match = /^(?<name>[a-z ]+) bags contain (?<child_rules>[a-z0-9, ]+)\.$/.match(@raw_rule)
-    { match['name'] => child_rules(match['child_rules']) }
+    { name => child_rules }
+  end
+
+  def parse_name_and_rules (raw_rule)
+    match = /^(?<name>[a-z ]+) bags contain (?<child_rules>[a-z0-9, ]+)\.$/.match(raw_rule)
+    [ match['name'], parse_child_rules(match['child_rules']) ]
   end
   
-  def child_rules (raw_child_rules)
+  def parse_child_rules (raw_child_rules)
     cr = {}
     raw_child_rules.split(',').map(&:strip).each do |rcr|
       match = /(?<empty>no other)|(?<number>\d+) (?<name>[a-z ]+) bag/.match(rcr)
