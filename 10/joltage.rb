efb18efb145103fd8671def18e60d7a611aa6f08
Sam Johnson <sam@samwich.com>
2020-12-10 15:28:52 -0800

    tests are passing

--- 10/joltage.rb
+++ 10/joltage.rb
@@ -3,6 +3,8 @@ class Joltage
     File.open(file_name) do |f|
       @input = f.each_line.map(&:to_i)
     end
+    @input.append(0) # charging outlet
+    @tree = make_tree(@input)
   end
   
   def process_file
@@ -10,7 +12,6 @@ class Joltage
   end
   
   def process (input)
-    input.append(0) # charging outlet
     adaptors = input.sort
     distribution = Hash.new { |h,k| h[k] = 0 }
     adaptors.each_with_index do |a, i|
@@ -20,27 +21,39 @@ class Joltage
         difference = adaptors[i+1] - a
       end
       distribution[difference] += 1
-      puts "#{a} + #{difference} = #{a + difference}"
+      # puts "#{a} + #{difference} = #{a + difference}"
     end
     distribution
   end
   
-  def to_tree
-    make_tree adaptors
-  end
-  
   def make_tree (input)
-    input.append(0) # charging outlet
     adaptors = input.sort
-    tree = Hash.new { |h,k| h[k] = [] }
+    adaptors << adaptors.last + 3 # my device is +3
+    tree = Hash.new { |h,k| h[k] = { children: [] } }
     adaptors.each_with_index do |a,i|
       adaptors[i+1, 3].each do |candidate|
         if candidate <= (a + 3)
-          tree[a] << candidate
+          tree[a][:children] << candidate
         end
       end
     end
+    tree[adaptors.last][:children] = []
     tree
+  end
+  
+  def path_count (node=0)
+    if @tree[node][:path_count]
+      return @tree[node][:path_count]
+    end
+    count = 0
+    children = @tree[node][:children]
+    if children.nil?
+      @tree[node][:path_count] = count
+      return count
+    end
+    children.reduce(0) do |sum, c|
+      sum + path_count(c)
+    end
   end
   
 end
--- 10/test_day10.rb
+++ 10/test_day10.rb
@@ -4,7 +4,7 @@ require_relative "joltage"
 
 class TestJoltage < Test::Unit::TestCase
   def test_one
-    input = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
+    input = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4] << 0 # add the outlet
     j = Joltage.new('./test_input')
     assert_equal({1=>7,3=>5}, j.process(input))
   end
@@ -15,19 +15,21 @@ class TestJoltage < Test::Unit::TestCase
   end
   
   def test_make_tree
-    input = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4]
+    input = [16, 10, 15, 5, 1, 11, 7, 19, 6, 12, 4] << 0 # add the outlet
     output = {
-      0=>[1],
-      1=>[4],
-      4=>[5, 6, 7],
-      5=>[6, 7],
-      6=>[7],
-      7=>[10],
-      10=>[11, 12],
-      11=>[12],
-      12=>[15],
-      15=>[16],
-      16=>[19]
+      0 =>{children: [1]},
+      1 =>{children: [4]},
+      4 =>{children: [5, 6, 7]},
+      5 =>{children: [6, 7]},
+      6 =>{children: [7]},
+      7 =>{children: [10]},
+      10=>{children: [11, 12]},
+      11=>{children: [12]},
+      12=>{children: [15]},
+      15=>{children: [16]},
+      16=>{children: [19]},
+      19=>{children: [22]},
+      22=>{children: []},
     }
     j = Joltage.new('./test_input')
     assert_equal(output, j.make_tree(input))
