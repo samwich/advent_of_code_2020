ac3cf99121e1fa6f8cdd772e8dca92003938075d
Sam Johnson <sam@samwich.com>
2020-12-10 23:41:42 -0800

    day 11 part 1 first iteration test passing

--- 11/seating.rb
+++ 11/seating.rb
@@ -2,12 +2,61 @@ class Seating
   attr_reader :before
   
   def initialize (file_name)
-    @even_board = read_file(file_name)
-    @odd_board = []
-    
-    @before = @even_board
-    @after = @odd_board
+    @before = read_file(file_name)
+    @after = Array.new(@before.length) { Array.new(@before.first.length) }
     @iteration = 0
+  end
+  
+  def neighborhood (row, column)
+    result = []
+    if @before[row - 1]
+      result << @before[row - 1][column - 1, 3]
+    end
+    result << [@before[row][column - 1], @before[row][column + 1]]
+    if @before[row + 1]
+      result << @before[row + 1][column - 1, 3]
+    end
+    result
+  end
+  
+  def value_for (row, column)
+    cell_value = @before[row][column]
+    return nil if cell_value.nil? # empty floor space
+    
+    neighbor_count = neighborhood(row, column).flatten.compact.sum
+    
+    # puts "cell_value #{cell_value} neighbor_count #{neighbor_count}"
+    if cell_value == 0 && neighbor_count == 0
+      1
+    elsif cell_value = 1 &&  neighbor_count >= 4
+      0
+    else
+      cell_value
+    end
+  end
+  
+  def process_board
+    # iterate over cells, assigning values for each
+    @before.each_with_index do |row, row_i|
+      puts "Row #{row_i}"
+      row.each_index do |column|
+        new_value = value_for(row_i, column)
+        @after[row_i][column] = new_value
+      end
+    end
+    
+    # compare before and after boards for stability
+    if @before == @after
+      puts "no change"
+      return @iteration
+    end
+    # swap before and after boards
+    @before, @after = @after, @before
+    @iteration += 1
+  end
+  
+  def next
+    
   end
   
   def read_file (file_name)
@@ -28,14 +77,6 @@ class Seating
         
       end
     end
-  end
-  
-  def neighborhood (x,y)
-    
-  end
-  
-  def next
-    
   end
   
 end
--- 11/test_day11.rb
+++ 11/test_day11.rb
@@ -8,4 +8,12 @@ class TestSeating < Test::Unit::TestCase
     assert_equal(10, s.before.length)
     assert_equal(10, s.before.first.length)
   end
+  
+  def test_iteration_1
+    s = Seating.new('./test0')
+    after = s.read_file('./test1')
+    # pp after
+    s.process_board
+    assert_equal(after, s.before)
+  end
 end
