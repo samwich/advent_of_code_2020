472849ab6d2f06684207c968e2e368351e4e2da2
Sam Johnson <sam@samwich.com>
2020-12-13 17:06:20 -0800

    day 13 part 2 brute force approach

--- 13/schedule.rb
+++ 13/schedule.rb
@@ -1,11 +1,14 @@
 class Schedule
   attr_reader :bus_ids, :earliest_departure_time
+  attr_accessor :buses_with_x
+
   def initialize (file_name)
     File.open(file_name) do |f|
       lines = f.readlines
       raise "input file problem" unless lines[2].nil?
       @earliest_departure_time = lines[0].to_i
       @bus_ids = lines[1].split(',').reject { |x| x == 'x' }.map(&:to_i)
+      @buses_with_x = lines[1].split(',').map(&:to_i)
     end
   end
 
@@ -23,4 +26,29 @@ class Schedule
     end
   end
 
+  def find_subsequent
+    result = false
+    (0..).each do |i|
+      time = @buses_with_x.first * i
+      @buses_with_x.each_with_index do |bus, i|
+        next if bus == 0
+        if ((time + i) % bus) == 0
+          result = time
+        else
+          result = false
+          break
+        end
+      end
+      
+      if i % 10_000 == 0
+        puts time
+      end
+
+      if result
+        return result
+      else
+        next
+      end
+    end
+  end
 end
--- 13/test_day13.rb
+++ 13/test_day13.rb
@@ -14,4 +14,27 @@ class TestSchedule < Test::Unit::TestCase
     answer = result[:bus] * (result[:time] - s.earliest_departure_time)
     assert_equal(295, answer)
   end
+
+  def test_buses_with_x
+    s = Schedule.new('./test_input')
+    assert_equal(8, s.buses_with_x.length)
+  end
+
+  def test_successors
+    s = Schedule.new('./test_input')
+    s.buses_with_x = [17,0,13,19]
+    assert_equal(3417, s.find_subsequent)
+    # 67,7,59,61 first occurs at timestamp 754018.
+    # 67,x,7,59,61 first occurs at timestamp 779210.
+    # 67,7,x,59,61 first occurs at timestamp 1261476.
+    # 1789,37,47,1889 first occurs at timestamp 1202161486.
+    s.buses_with_x = [67,7,59,61]
+    assert_equal(754018, s.find_subsequent)
+    s.buses_with_x = [67,0,7,59,61]
+    assert_equal(779210, s.find_subsequent)
+    s.buses_with_x = [67,7,0,59,61]
+    assert_equal(1261476, s.find_subsequent)
+    s.buses_with_x = [1789,37,47,1889]
+    assert_equal(1202161486, s.find_subsequent)
+  end
 end
