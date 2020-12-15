87d93ae7092ac564c69e0e509209e704fa6a6dd6
Sam Johnson <sam@samwich.com>
2020-12-15 12:22:49 -0800

    day 15 part 2 complete ruby day15.rb  11.21s user 0.17s system 99% cpu 11.402 total MBP (13-inch,M1,2020) ruby 2.6.3p62 (2019-04-16 revision 67580) [universal.arm64e-darwin20]

--- 15/day15.rb
+++ 15/day15.rb
@@ -44,4 +44,21 @@ mg = MemoryGame.new([1,2,16,19,18,0])
 puts "Day 15 Part 1: #{mg.play}"
 
 # Your puzzle answer was 536.
+#
+# --- Part Two ---
+#
+# Impressed, the Elves issue you a challenge: determine the 30000000th number spoken. For example, given the same starting numbers as above:
+#
+# Given 0,3,6, the 30000000th number spoken is 175594.
+# Given 1,3,2, the 30000000th number spoken is 2578.
+# Given 2,1,3, the 30000000th number spoken is 3544142.
+# Given 1,2,3, the 30000000th number spoken is 261214.
+# Given 2,3,1, the 30000000th number spoken is 6895259.
+# Given 3,2,1, the 30000000th number spoken is 18.
+# Given 3,1,2, the 30000000th number spoken is 362.
+# Given your starting numbers, what will be the 30000000th number spoken?
 
+mg = MemoryGame.new([1,2,16,19,18,0], 30_000_000)
+puts "Day 15 Part 2: #{mg.play}"
+
+# Your puzzle answer was 24065124.
