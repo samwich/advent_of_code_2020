45ebb086166ead57c01b07cfc805b5384e8c138a
Sam Johnson <sam@samwich.com>
2020-12-19 13:23:02 -0800

    day 18 part 1 complete

--- 18/day18.rb
+++ 18/day18.rb
@@ -1,21 +1,110 @@
+# --- Day 18: Operation Order ---
+#
+# As you look out the window and notice a heavily-forested continent slowly appear over the horizon, you are interrupted by the child sitting next to you. They're curious if you could help them with their math homework.
+#
+# Unfortunately, it seems like this "math" follows different rules than you remember.
+#
+# The homework (your puzzle input) consists of a series of expressions that consist of addition (+), multiplication (*), and parentheses ((...)). Just like normal math, parentheses indicate that the expression inside must be evaluated before it can be used by the surrounding expression. Addition still finds the sum of the numbers on both sides of the operator, and multiplication still finds the product.
+#
+# However, the rules of operator precedence have changed. Rather than evaluating multiplication before addition, the operators have the same precedence, and are evaluated left-to-right regardless of the order in which they appear.
+#
+# For example, the steps to evaluate the expression 1 + 2 * 3 + 4 * 5 + 6 are as follows:
+#
+# 1 + 2 * 3 + 4 * 5 + 6
+#   3   * 3 + 4 * 5 + 6
+#       9   + 4 * 5 + 6
+#          13   * 5 + 6
+#              65   + 6
+#                  71
+# Parentheses can override this order; for example, here is what happens if parentheses are added to form 1 + (2 * 3) + (4 * (5 + 6)):
+#
+# 1 + (2 * 3) + (4 * (5 + 6))
+# 1 +    6    + (4 * (5 + 6))
+#      7      + (4 * (5 + 6))
+#      7      + (4 *   11   )
+#      7      +     44
+#             51
+# Here are a few more examples:
+#
+# 2 * 3 + (4 * 5) becomes 26.
+# 5 + (8 * 3 + 9 + 3 * 4 * 3) becomes 437.
+# 5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4)) becomes 12240.
+# ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2 becomes 13632.
+# Before you can help with the homework, you need to understand it yourself. Evaluate the expression on each line of the homework; what is the sum of the resulting values?
+#
+
 require 'bundler/setup'
 require 'parslet'
 require_relative 'tutor'
 
-File.open('./test_input') do |f|
-  @problems = f.readlines
-end
+CHAR_MAP = {
+  ' ' => ',',
+  '(' => '[',
+  ')' => ']',
+  '*' => ':*',
+  '+' => ':+',
+}
 
-
-@problems.each do |prob|
-  pp prob
-  t = Tutor.new
-  begin
-    t.parse(prob)
-  rescue Parslet::ParseFailed => failure
-    puts failure.parse_failure_cause.ascii_tree
+File.open('./input') do |f|
+  @problems = f.readlines.map do |prob|
+    eval('[' + prob.gsub!(/[*+ \(\)]/, CHAR_MAP).chomp + ']')
   end
-  # pp t
+
 end
 
+
+def solve(prob)
+  prob.each_with_index do |e, i|
+    if e.instance_of? Array
+      prob[i] = solve(e)
+    end
+  end
+
+  while prob.length > 1
+    exp = prob.shift(3)
+    prob.unshift( exp[0].send(exp[1], exp[2]) )
+  end
+
+  prob.first
+end
+
+result = @problems.map do |prob|
+  pp prob
+  solve(prob)
+end
+
+puts result.sum
+
+# Your puzzle answer was 14006719520523.
+#
+# The first half of this puzzle is complete! It provides one gold star: *
+#
+# --- Part Two ---
+#
+# You manage to answer the child's questions and they finish part 1 of their homework, but get stuck when they reach the next section: advanced math.
+#
+# Now, addition and multiplication have different precedence levels, but they're not the ones you're familiar with. Instead, addition is evaluated before multiplication.
+#
+# For example, the steps to evaluate the expression 1 + 2 * 3 + 4 * 5 + 6 are now as follows:
+#
+# 1 + 2 * 3 + 4 * 5 + 6
+#   3   * 3 + 4 * 5 + 6
+#   3   *   7   * 5 + 6
+#   3   *   7   *  11
+#      21       *  11
+#          231
+# Here are the other examples from above:
+#
+# 1 + (2 * 3) + (4 * (5 + 6)) still becomes 51.
+# 2 * 3 + (4 * 5) becomes 46.
+# 5 + (8 * 3 + 9 + 3 * 4 * 3) becomes 1445.
+# 5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4)) becomes 669060.
+# ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2 becomes 23340.
+# What do you get if you add up the results of evaluating the homework problems using these new rules?
+#
+# Answer:
+#
+# Although it hasn't changed, you can still get your puzzle input.
+#
+# You can also [Share] this puzzle.
 
