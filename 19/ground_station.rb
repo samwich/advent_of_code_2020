b6bde3b427995ed7438cf0c0f8b5e279078f1123
Sam Johnson <sam@samwich.com>
2020-12-19 15:33:08 -0800

    rule reading tests passing

--- 19/ground_station.rb
+++ 19/ground_station.rb
@@ -1,5 +1,46 @@
 class GroundStation
   def initialize (file_name)
+    @rules = []
+    File.open(file_name) do |f|
+      while true
+        l = f.readline
+        break if l.strip.empty?
+        idx, rules = parse_rule(l)
+        @rules[idx] = rules
+      end
+      
+      @messages = []
+      while true
+        begin
+          l = f.readline
+          @messages << l.chomp
+        rescue EOFError
+          break
+        end
+      end
+    end
     
+    # pp @rules
+    pp @messages
+  end
+  
+  def parse_rule (line)
+    # puts line
+    matches = /^(?<addr>\d+): ("(?<char>[ab])"$|(?<list1>\d( \d)*))( \| (?<list2>\d( \d)*))?/.match(line)
+    # pp matches
+    
+    addr = matches[:addr].to_i
+    
+    references = [matches[:list1], matches[:list2]].compact.map do |list|
+      list.split(' ').map(&:to_i)
+    end
+    
+    if matches[:char]
+      rules = [matches[:char]]
+    else
+      rules = references
+    end
+    
+    [ addr, rules ]
   end
 end
--- 19/test_day19.rb
+++ 19/test_day19.rb
@@ -3,7 +3,39 @@ require "test/unit"
 require_relative "ground_station"
 
 class TestDay19 < Test::Unit::TestCase
-  def test_case_name
-    
+  def setup
+    @gs = GroundStation.new('./test_input')
   end
+  
+  def test_read_file
+  end
+  
+  def test_parse_single_character
+    assert_equal([1, ['a']], @gs.parse_rule('1: "a"'))
+  end
+  
+  def test_parse_rule0
+    assert_equal([0, [[4, 1, 5]]], @gs.parse_rule("0: 4 1 5\n"))
+  end
+  
+  def test_parse_rule1
+    assert_equal([1, [[2,3],[3,2]]], @gs.parse_rule("1: 2 3 | 3 2\n"))
+  end
+  
+  def test_parse_rule2
+    assert_equal([2, [[4,4],[5,5]]], @gs.parse_rule("2: 4 4 | 5 5\n"))
+  end
+  
+  def test_parse_rule3
+    assert_equal([3, [[4,5],[5,4]]], @gs.parse_rule('3: 4 5 | 5 4'))
+  end
+  
+  def test_parse_rule4
+    assert_equal([4, ['a']], @gs.parse_rule('4: "a"'))
+  end
+  
+  def test_parse_rule5
+    assert_equal([5, ['b']], @gs.parse_rule('5: "b"'))
+  end
+
 end
