0c30322431b9ea7f779c620a9ad530aff71cf9fe
Sam Johnson <sam@samwich.com>
2020-12-19 11:51:36 -0800

    parslet experiments

--- 18/tutor.rb
+++ 18/tutor.rb
@@ -9,10 +9,53 @@ class Tutor < Parslet::Parser
   rule(:integer)    { match('[0-9]').repeat(1).as(:int) >> space? }
   rule(:operator)   { match('[+*]').as(:op) >> space? }
 
+  rule(:parens)     { lparen >> expression.as(:expression) >> rparen }
+  rule(:factor)     { parens | integer }
+  rule(:expression) { factor >> ((operator >> factor).repeat(0)).maybe }
+
+  # rule(:expression) { expression >> '*' >> term || term }
+  # rule(:term)       { term >> '+' >> atom || atom }
+  # rule(:atom)       { integer || lparen >> expr >> rparen }
   
-  # grammar
-  rule(:factor)      { integer | lparen >> expression.as(:expression) >> rparen }
-  rule(:expression)  { factor >> (operator >> factor).repeat(0) }
+  # rule(:expr) { expr >> '+' >> term || expr >> '-' >> term || term }
+  # rule(:term) { term >> '*' >> atom || term >> '/' >> atom || atom }
+  # rule(:atom) { integer || lparen >> expr >> rparen }
+
+  # taken from guido van rossums beuilding a PEG series:
+  # statement: assignment | expr | if_statement
+  # expr: expr '+' term | expr '-' term | term
+  # term: term '*' atom | term '/' atom | atom
+  # atom: NAME | NUMBER | '(' expr ')'
+  # assignment: target '=' expr
+  # target: NAME
+  # if_statement: 'if' expr ':' statement
+  
+  
+  
   
   root(:expression)
+end
+
+IntLit = Struct.new(:int) do
+  def eval
+    int.to_i
+  end
+end
+
+Calculation = Struct.new(:left, :operator, :right) do
+  def eval
+    case operator
+    when '+'
+      left.eval + right.eval
+    when '*'
+      left.eval * right.eval
+    end
+  end
+end
+
+class Transformer < Parslet::Transform
+  rule(:int => simple(:int))        { IntLit.new(int) }
+  rule(:left => simple(:left),
+       :right => simple(:right),
+       :op => simple(:op))          { Calculation.new(left, op, right)}
 end
