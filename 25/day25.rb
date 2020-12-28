55b438a730f810a84dd9592b41a04eb5a242a5ae
Sam Johnson <sam@samwich.com>
2020-12-28 12:50:40 -0800

    day 25 part 2 complete

--- 25/day25.rb
+++ 25/day25.rb
@@ -45,3 +45,13 @@ puts "Day 25 Part 1: #{ComboBreaker.new(input).encryption_key}"
 
 # Your puzzle answer was 11576351.
 
+# --- Part Two ---
+# 
+# The light turns green and the door unlocks. As you collapse onto the bed in your room, your pager goes off!
+# 
+# "It's an emergency!" the Elf calling you explains. "The soft serve machine in the cafeteria on sub-basement 7 just failed and you're the only one that knows how to fix it! We've already dispatched a reindeer to your location to pick you up."
+# 
+# You hear the sound of hooves landing on your balcony.
+# 
+# The reindeer carefully explores the contents of your room while you figure out how you're going to pay the 50 stars you owe the resort before you leave.
+# ...
